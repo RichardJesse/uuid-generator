3794d35eef681711e707bf5512c7254dcc136859
RichardJesse <jesse.richard@strathmore.edu>
2024-03-02 12:18:40 -0800

    Fixed the issue with the undefined uuid generation

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // this line makes sure that the uuids do not repeat
 var existingUUIDs = new Set();
-
-
+ 
+/**
+ *  Generates a version 4 UUID and checks if the ID 
+ *  has already been given before giving the next address
+ * @returns 
+ */
 function generateUID() {
     var uid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
         var r = Math.random() * 16 | 0,
@@ -17,7 +21,7 @@ function generateUID() {
 
     existingUUIDs.add(uid);
 
-    console.log(uid);
+    return uid
 }
 
 module.exports = generateUID;
